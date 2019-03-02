9d84331e8049101a48d86f47bc790edf90b129df
Youssef Roshdy <uossefkhaled99@gmail.com>
2019-03-02 16:41:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+css.textContent = "linear-gradient(to right, red , yellow);";
+function setGradient(color1, color2) {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient(color1, color2));
+
+color2.addEventListener("input", setGradient(color1, color2));
+
+/* Random Color */
+function randomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function addRandomColor() {
+  color1.value = randomColor();
+  color2.value = randomColor();
+
+  setGradient(color1, color2);
+}
